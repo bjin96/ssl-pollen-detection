fc37814d873afa12f5918f5f128ca1700d102f52
Benjamin Jin <benjaminmjin@gmail.com>
2022-05-23 11:45:52 +0200

    Add evaluation

---
+++ src/evaluation/confusion_matrix.py
@@ -0,0 +1,122 @@
+# Copied from https://github.com/kaanakan/object_detection_confusion_matrix
+#
+#
+# MIT License
+#
+# Copyright (c) 2020 Kaan Akan
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+import numpy as np
+
+
+def box_iou_calc(boxes1, boxes2):
+    # https://github.com/pytorch/vision/blob/master/torchvision/ops/boxes.py
+    """
+    Return intersection-over-union (Jaccard index) of boxes.
+    Both sets of boxes are expected to be in (x1, y1, x2, y2) format.
+    Arguments:
+        boxes1 (Array[N, 4])
+        boxes2 (Array[M, 4])
+    Returns:
+        iou (Array[N, M]): the NxM matrix containing the pairwise
+            IoU values for every element in boxes1 and boxes2
+
+    This implementation is taken from the above link and changed so that it only uses numpy..
+    """
+
+    def box_area(box):
+        # box = 4xn
+        return (box[2] - box[0]) * (box[3] - box[1])
+
+    area1 = box_area(boxes1.T)
+    area2 = box_area(boxes2.T)
+
+    lt = np.maximum(boxes1[:, None, :2], boxes2[:, :2])  # [N,M,2]
+    rb = np.minimum(boxes1[:, None, 2:], boxes2[:, 2:])  # [N,M,2]
+
+    inter = np.prod(np.clip(rb - lt, a_min=0, a_max=None), 2)
+    return inter / (area1[:, None] + area2 - inter)  # iou = inter / (area1 + area2 - inter)
+
+
+class ConfusionMatrix:
+    def __init__(self, num_classes: int, CONF_THRESHOLD=0.3, IOU_THRESHOLD=0.5):
+        self.matrix = np.zeros((num_classes + 1, num_classes + 1))
+        self.num_classes = num_classes
+        self.CONF_THRESHOLD = CONF_THRESHOLD
+        self.IOU_THRESHOLD = IOU_THRESHOLD
+
+    def process_batch(self, detections, labels: np.ndarray):
+        """
+        Return intersection-over-union (Jaccard index) of boxes.
+        Both sets of boxes are expected to be in (x1, y1, x2, y2) format.
+        Arguments:
+            detections (Array[N, 6]), x1, y1, x2, y2, conf, class
+            labels (Array[M, 5]), class, x1, y1, x2, y2
+        Returns:
+            None, updates confusion matrix accordingly
+        """
+        gt_classes = labels[:, 0].astype(np.int16)
+
+        try:
+            detections = detections[detections[:, 4] > self.CONF_THRESHOLD]
+        except IndexError or TypeError:
+            # detections are empty, end of process
+            for i, label in enumerate(labels):
+                gt_class = gt_classes[i]
+                self.matrix[self.num_classes, gt_class] += 1
+            return
+
+        detection_classes = detections[:, 5].astype(np.int16)
+
+        all_ious = box_iou_calc(labels[:, 1:], detections[:, :4])
+        want_idx = np.where(all_ious > self.IOU_THRESHOLD)
+
+        all_matches = [[want_idx[0][i], want_idx[1][i], all_ious[want_idx[0][i], want_idx[1][i]]]
+                       for i in range(want_idx[0].shape[0])]
+
+        all_matches = np.array(all_matches)
+        if all_matches.shape[0] > 0:  # if there is match
+            all_matches = all_matches[all_matches[:, 2].argsort()[::-1]]
+
+            all_matches = all_matches[np.unique(all_matches[:, 1], return_index=True)[1]]
+
+            all_matches = all_matches[all_matches[:, 2].argsort()[::-1]]
+
+            all_matches = all_matches[np.unique(all_matches[:, 0], return_index=True)[1]]
+
+        for i, label in enumerate(labels):
+            gt_class = gt_classes[i]
+            if all_matches.shape[0] > 0 and all_matches[all_matches[:, 0] == i].shape[0] == 1:
+                detection_class = detection_classes[int(all_matches[all_matches[:, 0] == i, 1][0])]
+                self.matrix[detection_class, gt_class] += 1
+            else:
+                self.matrix[self.num_classes, gt_class] += 1
+
+        for i, detection in enumerate(detections):
+            if not all_matches.shape[0] or ( all_matches.shape[0] and all_matches[all_matches[:, 1] == i].shape[0] == 0):
+                detection_class = detection_classes[i]
+                self.matrix[detection_class, self.num_classes] += 1
+
+    def return_matrix(self):
+        return self.matrix
+
+    def print_matrix(self):
+        for i in range(self.num_classes + 1):
+            entries = [e.zfill(6) for e in map(str, self.matrix[i])]
+            print(' '.join(entries))
--- src/evaluation/plot_annotated_images.py
+++ src/evaluation/plot_annotated_images.py
@@ -1,9 +1,11 @@
+import pickle
 from typing import Type
 
 import numpy as np
 from torch.utils.data import DataLoader
 from pytorch_lightning import LightningModule
 
+from src.evaluation.confusion_matrix import ConfusionMatrix
 from src.models.soft_teacher import SoftTeacher
 from src.training.transforms import ToTensor
 import os
@@ -45,6 +47,101 @@ def plot_annotated_images(checkpoint_path: str, model_class: Type[LightningModul
         )
 
 
+def plot_from_results(path_a, path_b):
+    validation_dataset = Augsburg15DetectionDataset(
+        root_directory=os.path.join('../../datasets/pollen_only'),
+        image_info_csv='pollen15_val_annotations_preprocessed.csv',
+        transforms=ToTensor()
+    )
+    validation_loader = DataLoader(
+        validation_dataset,
+        batch_size=1,
+        collate_fn=collate_augsburg15_detection,
+        drop_last=True,
+        num_workers=4
+    )
+
+    with open(path_a, 'rb') as file:
+        a = pickle.load(file)
+
+    with open(path_b, 'rb') as file:
+        b = pickle.load(file)
+
+    for index, sample in enumerate(validation_loader):
+        image, target = sample
+
+        plot_bounding_box_image_scores(
+            image[0].detach().numpy(),
+            a[index][0].numpy(),
+            a[index][1].numpy(),
+            a[index][2].numpy(),
+            b[index][0].numpy(),
+            b[index][1].numpy(),
+            b[index][2].numpy(),
+            index
+        )
+
+
+def make_validation_predictions(checkpoint_path, model_class):
+    validation_dataset = Augsburg15DetectionDataset(
+        root_directory=os.path.join('../../datasets/pollen_only'),
+        image_info_csv='pollen15_val_annotations_preprocessed.csv',
+        transforms=ToTensor()
+    )
+    validation_loader = DataLoader(
+        validation_dataset,
+        batch_size=1,
+        collate_fn=collate_augsburg15_detection,
+        drop_last=True,
+        num_workers=4
+    )
+
+    model = model_class.load_from_checkpoint(
+        checkpoint_path,
+        num_classes=Augsburg15DetectionDataset.NUM_CLASSES,
+        batch_size=1
+    )
+    model.eval()
+
+    results = []
+
+    for index, sample in enumerate(validation_loader):
+        print(f'{index + 1}/{len(validation_dataset)}')
+        image, target = sample
+
+        result = model(image)
+
+        results.append((result[0]['boxes'], result[0]['labels'], result[0]['scores']))
+
+    with open('results.pkl', 'w') as file:
+        pickle.dump(results, file)
+
+
+def plot_bounding_box_image_scores(image, bounding_boxes, labels, scores, ground_truth_boxes, ground_truth_labels, ground_truth_scores, index):
+    scores_mask = scores > 0.5
+    ground_truth_scores_mask = ground_truth_scores > 0.5
+
+    bounding_boxes = bounding_boxes[scores_mask]
+    labels = labels[scores_mask]
+
+    ground_truth_boxes = ground_truth_boxes[ground_truth_scores_mask]
+    ground_truth_labels = ground_truth_labels[ground_truth_scores_mask]
+
+    if not is_interesting_image(bounding_boxes, labels, ground_truth_boxes, ground_truth_labels):
+        return
+
+    plt.rcParams["figure.figsize"] = (20, 20)
+    fig, ax = plt.subplots()
+
+    ax.imshow(image.transpose(1, 2, 0))
+
+    _plot_bounding_boxes(bounding_boxes, labels, scores, ax, 'green')
+    _plot_bounding_boxes(ground_truth_boxes, ground_truth_labels, ground_truth_scores, ax, 'red')
+
+    plt.savefig(f'../../plots/{index}.jpg')
+    plt.close()
+
+
 def plot_bounding_box_image(image, bounding_boxes, labels, scores, ground_truth_boxes, ground_truth_labels, index):
     plt.rcParams["figure.figsize"] = (20, 20)
     fig, ax = plt.subplots()
@@ -56,6 +153,37 @@ def plot_bounding_box_image(image, bounding_boxes, labels, scores, ground_truth_
 
     plt.savefig(f'../../plots/{index}.jpg')
     plt.close()
+
+
+def is_interesting_image(a_boxes, a_labels, b_boxes, b_labels):
+    for box, label in zip(a_boxes, a_labels):
+        if is_interesting_box(box, label, b_boxes, b_labels):
+            return True
+
+    for box, label in zip(b_boxes, b_labels):
+        if is_interesting_box(box, label, a_boxes, a_labels):
+            return True
+
+    return False
+
+
+def is_interesting_box(box, label, other_boxes, other_labels):
+    for other_box, other_label in zip(other_boxes, other_labels):
+        vertical_start_inside = other_box[0] <= box[0] <= other_box[2]
+        vertical_end_inside = other_box[0] <= box[2] <= other_box[2]
+        vertical_enclosing = box[0] <= other_box[0] <= box[2]
+        vertical_overlap = vertical_start_inside or vertical_end_inside or vertical_enclosing
+
+        horizontal_start_inside = other_box[1] <= box[1] <= other_box[3]
+        horizontal_end_inside = other_box[1] <= box[3] <= other_box[3]
+        horizontal_enclosing = box[1] <= other_box[1] <= box[3]
+        horizontal_overlap = horizontal_start_inside or horizontal_end_inside or horizontal_enclosing
+
+        if vertical_overlap and horizontal_overlap:
+            if label == other_label:
+                return False
+
+    return True
 
 
 def _plot_bounding_boxes(bounding_boxes, labels, scores, ax, color):
@@ -84,6 +212,72 @@ def _plot_bounding_boxes(bounding_boxes, labels, scores, ax, color):
         )
 
 
+def count_bounding_boxes(path, p):
+    with open(path, 'rb') as file:
+        a = pickle.load(file)
+
+    count = 0
+
+    for boxes, labels, scores in a:
+        count += len([score for score in scores if score > p])
+
+    return count
+
+
+def get_confusion_matrix(path):
+    validation_dataset = Augsburg15DetectionDataset(
+        root_directory=os.path.join('../../datasets/pollen_only'),
+        image_info_csv='pollen15_val_annotations_preprocessed.csv',
+        transforms=ToTensor()
+    )
+    validation_loader = DataLoader(
+        validation_dataset,
+        batch_size=1,
+        collate_fn=collate_augsburg15_detection,
+        drop_last=True,
+        num_workers=4
+    )
+
+    with open(path, 'rb') as file:
+        a = pickle.load(file)
+
+    conf_mat = ConfusionMatrix(num_classes=15, CONF_THRESHOLD=0.5, IOU_THRESHOLD=0.5)
+
+    for i, ((_, target), (boxes, labels, scores)) in enumerate(zip(validation_loader, a)):
+        print(f'{i}/{len(a)}')
+        preds = np.array([[*box, score, label - 1] for box, label, score in zip(boxes, labels, scores)])
+        gt = np.array([[label - 1, *box] for box, label in zip(target[0]['boxes'], target[0]['labels'])])
+        conf_mat.process_batch(preds, gt)
+
+    return conf_mat
+
+
+def compare_confusion_matrix(path_a, path_b):
+    matrix_baseline = get_confusion_matrix(path_a)
+    matrix_ssl = get_confusion_matrix(path_b)
+
+    print('baseline: ')
+    matrix_baseline.print_matrix()
+    print(f'FP: {matrix_baseline.return_matrix()[:, -1].sum()}')
+    print(f'FN: {matrix_baseline.return_matrix()[-1, :].sum()}')
+
+    print('ssl:')
+    matrix_ssl.print_matrix()
+    print(f'FP: {matrix_ssl.return_matrix()[:, -1].sum()}')
+    print(f'FN: {matrix_ssl.return_matrix()[-1, :].sum()}')
+
+    print()
+
+
 if __name__ == '__main__':
-    CKPT_PATH = '../../lightning_logs/soft_teacher_loss_weights#25b59bef/checkpoints/epoch=15-step=11807.ckpt'
-    plot_annotated_images(CKPT_PATH, SoftTeacher)
+    ssl_results = '/Users/benni/Desktop/ma/evaluation/ssl-results.pkl'
+    baseline_results = '/Users/benni/Desktop/ma/evaluation/baseline-results.pkl'
+
+    # CKPT_PATH = '/Users/benni/Desktop/ma/cluster_results/ssl-baseline/epoch=9-step=29550.ckpt'
+    # plot_annotated_images(CKPT_PATH, SoftTeacher)
+    # make_validation_predictions(CKPT_PATH, SoftTeacher)
+    plot_from_results(ssl_results, baseline_results)
+
+    # print(count_bounding_boxes(baseline_results, 0.0))
+
+    # compare_confusion_matrix(baseline_results, ssl_results)
