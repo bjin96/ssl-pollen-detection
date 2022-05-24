798c83d47798af41cf71afb4fb492bc024e3332a
Benjamin Jin <benjaminmjin@gmail.com>
2022-05-25 00:24:38 +0200

    Fix EMA updates
    model.parameters() does not contain all variables.

--- src/models/exponential_moving_average.py
+++ src/models/exponential_moving_average.py
@@ -1,4 +1,3 @@
-import torch
 from torch import no_grad
 from torch.nn import Module
 
@@ -17,10 +16,13 @@ class ExponentialMovingAverage(Module):
         super().__init__()
         self.decay = decay
 
-        self.student_parameters = list(student.parameters())
-        self.teacher_parameters = list(teacher.parameters())
+        self.student = student
+        self.teacher = teacher
 
     @no_grad()
     def update_teacher(self):
-        for teacher_parameter, student_parameter in zip(self.teacher_parameters, self.student_parameters):
+        for teacher_parameter, student_parameter in zip(self.teacher.parameters(), self.student.parameters()):
             teacher_parameter.data.copy_(self.decay * teacher_parameter + (1 - self.decay) * student_parameter)
+
+        for teacher_buffer, student_buffer in zip(self.teacher.buffers(), self.student.buffers()):
+            teacher_buffer.data.copy_(self.decay * teacher_buffer + (1 - self.decay) * student_buffer)
